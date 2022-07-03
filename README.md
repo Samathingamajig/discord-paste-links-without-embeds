34025c185386b7f788bd5f469ef9d29f117dfbf2
Samathingamajig <samathingamajig@gmail.com>
2022-07-03 15:23:47 -0500

    Version 0.2

--- README.md
+++ README.md
@@ -1,18 +1,34 @@
 # discord-paste-links-without-embeds
+
 Wraps URLs with angle brackets (&lt;https://example.com>) when pasting into discord to avoid link embeds
 
 ## 🐛 Bugs:
+
 Please file bugs under the [Issues tab](https://github.com/Samathingamajig/discord-paste-links-without-embeds/issues).
 
 ## ⚙ Config:
-There are two settings you can configure:
+
+There are a few settings you can configure:
+
 ```js
 const CONFIG = {
-  replaceStandaloneLinks: true, // will affect "https://stackoverflow.com", but not "check out https://stackoverflow.com"; default: true
-  replaceLinksInMessage: true, // will affect "check out https://stackoverflow.com", but not "https://stackoverflow.com"; default: true
+  replaceStandaloneLinks: true, // will affect "https://stackoverflow.com", but not "check out https://stackoverflow.com"; can be true or false; default: true
+  replaceLinksInMessage: true, // will affect "check out https://stackoverflow.com", but not "https://stackoverflow.com"; can be true or false; default: true
+  linkRegexProvider: "discord", // discord doesn't parse links perfectly on the frontend; can be "discord" or "custom"; default: "discord"
+  ignoreCaseInLink: true, // discord doesn't treat HTTPS as a valid link, unless you wrap in <>; can be false or true; default true
 };
 ```
 
-Note that these settings are not mutually exclusive. When both are `true` (the default setting), links will be affected regardless of if they're the entire clipboard or inside of the clipboard.
+Note that "replaceStandaloneLinks" and "replaceLinksInMessage" are not mutually exclusive. When both are `true` (the default setting), links will be affected regardless of if they're the entire clipboard or inside of the clipboard.
 
-To turn off one or both of these, replace `true` with `false`. **NOTE:** Changes are only re-evaluated when discord is completely refreshed
+To turn off one or both of these, replace `true` with `false`.
+
+##### 📝 Note:
+
+- Changes are only re-evaluated when discord is completely refreshed
+- Configuration will be lost on update, if you have any ideas for how to persist this configuration, please let me know in the [Issues tab](https://github.com/Samathingamajig/discord-paste-links-without-embeds/issues).
+
+## 🤔 Known Oddities:
+
+- When pasting a link surrounded by some characters, the angle brackets won't be visible, but they're still there and prevent the embed.
+- When pasting a message with a link already surrounded by angle brackets, more angle brackets will be added, resulting in the same visual as the source paste except the link will not be hyperlinked or embedded.
--- discord-paste-links-without-embeds.user.js
+++ discord-paste-links-without-embeds.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Discord Paste Links without Embeds
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Wraps URLs with angle brackets (<https://example.com>) when pasting into discord to avoid link embeds
 // @author       Samathingamajig
-// @match        https://discord.com/channels/*/*
+// @match        https://discord.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=discord.com
 // @license      MIT
 // @grant        none
@@ -14,20 +14,26 @@
   "use strict";
 
   // Config for the script
-  // NOTE: Changes are only re-evaluated when discord is completely refreshed
+  // NOTE: Changes are only re-evaluated when discord is completely refreshed, and are lost upon updates to the userscript
   const CONFIG = {
-    replaceStandaloneLinks: true, // will affect "https://stackoverflow.com", but not "check out https://stackoverflow.com"; default: true
-    replaceLinksInMessage: true, // will affect "check out https://stackoverflow.com", but not "https://stackoverflow.com"; default: true
+    replaceStandaloneLinks: true, // will affect "https://stackoverflow.com", but not "check out https://stackoverflow.com"; can be true or false; default: true
+    replaceLinksInMessage: true, // will affect "check out https://stackoverflow.com", but not "https://stackoverflow.com"; can be true or false; default: true
+    linkRegexProvider: "discord", // discord doesn't parse links perfectly on the frontend; can be "discord" or "custom"; default: "discord"
+    ignoreCaseInLink: true, // discord doesn't treat HTTPS as a valid link, unless you wrap in <>; can be false or true; default true
   };
 
   if ([CONFIG.replaceStandaloneLinks, CONFIG.replaceLinksInMessage].every((bool) => bool === false)) return;
 
   const linkRegexBase = () =>
-    /((?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?)/;
-  const linkRegexSingle = () => new RegExp("^" + linkRegexBase().source + "$", "i");
-  const linkRegexGlobal = () => new RegExp(linkRegexBase(), "gi");
+    CONFIG.linkRegexProvider.toLowerCase() === "discord"
+      ? /((?:https?|steam):\/\/[^\s<]+[^<.,:;"'\]\s])/
+      : /((?:(?:https?|ftp|steam):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?)/;
+  const linkRegexSingle = () => new RegExp("^" + linkRegexBase().source + "$", CONFIG.ignoreCaseInLink ? "i" : "");
+  const linkRegexGlobal = () => new RegExp(linkRegexBase(), "g" + (CONFIG.ignoreCaseInLink ? "i" : ""));
 
   const pasteEventListener = (event) => {
+    if (["textarea", "input", "br"].includes(event.target.nodeName.toLowerCase())) return;
+
     const paste = event.clipboardData?.getData("text");
     if (!paste || paste.length === 0) return;
     const oldGetData = event.clipboardData?.getData;
